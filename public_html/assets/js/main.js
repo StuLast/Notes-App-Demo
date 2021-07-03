f9e646a477fcdb3ae1738e91c4dd29401c029fce
Stu Last <stuart@theaudioprogrammer.com>
2021-07-03 17:48:11 +0100

    Implement live search

--- public_html/assets/js/main.js
+++ public_html/assets/js/main.js
@@ -1,34 +1,70 @@
-console.log("JS reporting for duty");
-
+//Setup temp data objects
+//=======================
 
 const notes = [
     {
-        title: 'Note B',
-        body: 'The Audio Programmer Sucks'
+        title: 'New Ideas',
+        body: 'Time to come up with new ideas'
     },
     {
-        title: 'Note 2',
-        body: 'Josh Sucks'
+        title: 'New Work',
+        body: 'Time to work on those new ideas'
     },
     {
-        title: 'Note A',
-        body: 'Josh is dishonest'
+        title: 'Get Income',
+        body: 'Time to generate income from those new ideas'
     },
 ];
 
-const getNotes = (notes) => {
-    notes.forEach((note) => {
-        renderNote(note);
-    })
+const filters = {
+    searchText: '',
 }
+
+//Dynamic data rendering
+//======================
 
 const renderNote = (note) => {
     const newNoteDiv = document.createElement('div');
+    newNoteDiv.className = "note";
     let html = "";
     html += `<h3>${note.title}</h3>`;
     html += `<p>${note.body}</p>`;
     newNoteDiv.innerHTML = html;
-    document.querySelector('#app').appendChild(newNoteDiv);
+    document.querySelector('#notes').appendChild(newNoteDiv);
 }
 
-getNotes(notes);
+const renderNotes = (notes, filters) => {
+    const filteredNotes = notes.filter((note) => {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase());
+    });
+    document.querySelector('#notes').innerHTML = "";
+    filteredNotes.forEach((note) => {
+        renderNote(note);
+    });
+};
+
+//On Startup
+
+renderNotes(notes, filters);
+
+//Event Listeners
+
+document.querySelector("#btn-add-note").addEventListener('click', (e) => {
+    e.preventDefault();
+    e.target.textContent = 'This button has been clicked';
+    console.log('Add Note button clicked');
+});
+
+document.querySelector('#btn-remove-all-notes').addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('Removing all notes');
+    document.querySelectorAll('#notes.note').forEach((note) => {
+        note.remove();
+    });
+});
+
+document.querySelector('#inp-search').addEventListener('input', (e) => {
+    e.preventDefault();
+    filters.searchText = e.target.value
+    renderNotes(notes, filters);
+})
